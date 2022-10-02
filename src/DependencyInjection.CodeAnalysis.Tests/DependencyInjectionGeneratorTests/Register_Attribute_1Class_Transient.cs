2c6142a63978409e68e3dd44006d39c130df54b1
Alexander Stärk <alexander.staerk@asprog.de>
2022-10-02 07:46:38 +0200

    #1 - test generated source for 1 class to register by attribute

---
+++ src/DependencyInjection.CodeAnalysis.Tests/DependencyInjectionGeneratorTests/Register_Attribute_1Class_Transient.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+
+namespace Basilisque.DependencyInjection.CodeAnalysis.Tests.DependencyInjectionGeneratorTests
+{
+    [TestClass]
+    public class Register_Attribute_1Class_Transient : BaseDependencyInjectionGeneratorTest
+    {
+        protected override void AddSourcesUnderTest(SourceFileList sources)
+        {
+            sources.Add(@"
+        /// <summary>
+        /// Test interface that will be registered as transient by the attribute
+        /// </summary>
+        public interface IMyPublicNotRegisteredClass
+        {
+        }
+");
+            sources.Add(@"
+        /// <summary>
+        /// Test class that will be registered as transient by the attribute
+        /// </summary>
+        [Basilisque.DependencyInjection.Registration.Annotations.RegisterServiceTransient]
+        public class MyPublicNotRegisteredClass : IMyPublicNotRegisteredClass
+        {
+        }
+        ");
+        }
+
+        protected override string? GetRegisteredServicesSource()
+        {
+            return @"
+            services.AddTransient<IMyPublicNotRegisteredClass, MyPublicNotRegisteredClass>();";
+        }
+    }
+}
+
