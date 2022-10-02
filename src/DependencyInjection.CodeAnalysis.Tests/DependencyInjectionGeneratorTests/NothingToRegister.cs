3b5453028f46d460bb6be98c18c570345320fbcd
Alexander Stärk <alexander.staerk@asprog.de>
2022-10-02 07:45:52 +0200

    #1 - test generated code when noting is found to register

---
+++ src/DependencyInjection.CodeAnalysis.Tests/DependencyInjectionGeneratorTests/NothingToRegister.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+
+namespace Basilisque.DependencyInjection.CodeAnalysis.Tests.DependencyInjectionGeneratorTests
+{
+    [TestClass]
+    public class NothingToRegister : BaseDependencyInjectionGeneratorTest
+    {
+        protected override void AddSourcesUnderTest(SourceFileList sources)
+        {
+            sources.Add(@"
+        /// <summary>
+        /// Test class that won't be registered to the dependency container
+        /// </summary>
+        public class MyPublicNotRegisteredClass
+        {
+        }
+        ");
+        }
+    }
+}
+
